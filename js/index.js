9fc473733dd78de247b893da4e6eb9deb2c982af
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-07-17 11:29:03 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,323 @@
+// 1-masala
+// let num = +prompt('son kirit')
+
+// if(num > 0){
+//     num++
+// }
+// console.log(num);
+
+
+// 2-masala
+// let num = +prompt('son kirirt')
+// if(num > 0){
+//     num++
+// } else{
+//     num -= 2
+// }
+// console.log(num);
+
+
+// 3-masala
+// let num = +prompt('son kirirt')
+// if(num > 0){
+//     num++
+// } else if(num = 10){
+//     num = 10
+// }
+// else{
+//     num -= 2
+// }
+// console.log(num);
+
+
+// 4-masala
+// let a = +prompt('son kirirt')
+// let b = +prompt('son kirirt')
+// let c = +prompt('son kirirt')
+// let res = 0
+// if(a > 0){
+//     res++
+// }
+// if(b > 0){
+//     res++
+// }
+// if(c > 0){
+//     res++
+// }
+// console.log(res);
+
+
+// 5-masala
+// let a = +prompt('son kirirt')
+// let b = +prompt('son kirirt')
+// let c = +prompt('son kirirt')
+// let res = 0
+// let man = 0
+// if(a > 0){
+//     res++
+// } else{
+//     man++
+// }
+// if(b > 0){
+//     res++
+// } else{
+//     man++
+// }
+// if(c > 0){
+//     res++
+// } else{
+//     man++
+// }
+// console.log(res);
+
+
+// 6-masala
+// let a = +prompt('son kirirt')
+// let b = +prompt('son kirirt')
+// if(a > b){
+//     max = a
+// } else {
+//     max = b
+// }
+// console.log(max);
+
+
+// 8-masala
+// let a = +prompt('son kirirt')
+// let b = +prompt('son kirirt')
+// if(a > b){
+//     max = a
+//     min = b
+// } else {
+//     max = b
+//     min = a
+// }
+// console.log(max);
+// console.log(min);
+
+
+// 9-masala
+// let a = +prompt('son kiriting')
+// let b = +prompt('son kiriting')
+// let c = 0
+// if(a > b){
+//     c = a
+//     a = b
+//     b = c
+// }
+// console.log(a)
+// console.log(b )
+
+
+// 10-masala
+// let a = +prompt('son kiriting')
+// let b = +prompt('son kiriting')
+// let res
+// if(a == b){
+//     res = a = b = 0
+// } else{
+//     res = a + b
+// }
+// console.log(res);
+
+
+// 12-masala
+// let a = +prompt('son kiriting')
+// let b = +prompt('son kiriting')
+// let c = +prompt('son kiriting')
+// let min 
+// if(a > b){
+//     min = b
+// } else{
+//     min = a
+// }
+// if(min > c){
+//     min = c
+// }
+// console.log(min);
+
+
+// 13-masala
+// let a = +prompt('son kiriting')
+// let b = +prompt('son kiriting')
+// let c = +prompt('son kiriting')
+// let mid 
+// if((a > b && a < c) || (a < b && a > c)){
+//     mid = a
+// } else if((a < b && b < c) || (a > b && b > c)){
+//     mid = b
+// } else{
+//     mid = c
+// }
+// console.log(mid);
+
+
+// 14-masala
+// let a = +prompt('son kiriting')
+// let b = +prompt('son kiriting')
+// let c = +prompt('son kiriting')
+// let min 
+// let max
+// if(a > b){
+//     max = a
+//     min = b
+// } else{
+//     max = b
+//     min = a
+// }
+// if(max < c){
+//     max = c
+// }
+// if(min > c){
+//     min = c
+// }
+// console.log(max);
+// console.log(min);
+
+// 15-masala
+// let a = +prompt('son kiriting')
+// let b = +prompt('son kiriting')
+// let c = +prompt('son kiriting')
+// let sum = 0
+// let min 
+// if(a > b){
+//     min = b
+// } else{
+//     min = a
+// }
+// if(min > c){
+//     min = c
+// }
+// sum = a + b + c - min
+// console.log(sum);
+
+
+
+// 1-masala
+// let a = +prompt('son kirit')
+// if(a > 0){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+// 2-masala
+// let a = +prompt('son kirit')
+// if(a % 2 == 1){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+// 3-masala
+// let a = +prompt('son kirit')
+// if(a % 2 == 0){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+// 4-masala
+// let a = +prompt('son kirit')
+// let b = +prompt('son kirirt')
+// if(a > 2 && b <= 3){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+// 5-masala
+// let a = +prompt('son kirirt')
+// let b = +prompt('son kirirt')
+// if(a >= 0 && b < -2){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+// 6-masala
+// let a = +prompt('son kirit')
+// let b = +prompt('son kirit')
+// let c = +prompt('son kirit')
+// if(a <= b && b <= c){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+// 7-msaala
+// let a = +prompt('son kirit')
+// let b = +prompt('son kirit')
+// let c = +prompt('son kirit')
+// if(a > b && b < c || a < b && b < c){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+// 8-masala
+// let a = +prompt('son kirit')
+// let b = +prompt('son kirit')
+// if(a % 2 == 1 && b % 2 == 1){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+// 9-masala
+// let a = +prompt('son kirit')
+// let b = +prompt('son kirit')
+// if(a % 2 == 1 || b % 2 == 0 || a % 2 == 0 || b % 2 == 1){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+// 10-masal
+// let a = +prompt('son kirit')
+// let b = +prompt('son kirit')
+// if(a % 2 == 1 && b % 2 == 0 || a % 2 == 0 && b % 2 == 1){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
